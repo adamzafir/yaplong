77abcb8e8b7b1db8d100b39a8043a4db1d22cb08
Chan Yap Long <chan.yap.long@sji.edu.sg>
2025-11-05 16:45:27 +0800

    Yes!

---
+++ lyiglyuvgylu/Screen2.swift
@@ -0,0 +1,63 @@
+//
+//  Screen2.swift
+//  lyiglyuvgylu
+//
+//  this part was coded by adam
+//
+
+import SwiftUI
+
+struct Screen2: View {
+    @Binding var title: String
+    @Binding var script: String
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("Unnamed Title", text: $title)
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .padding()
+                
+                TextEditor(text: $script)
+                    .padding(.horizontal)
+                
+                Spacer()
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("hello")
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct TheTabView: View {
+    @State private var title: String = "Unnamed Title"
+    @State private var script: String = "Type something..."
+    
+    var body: some View {
+        TabView {
+            Screen2(title: $title, script: $script)
+                .tabItem {
+                    Label("Editor", systemImage: "square.and.pencil")
+                }
+        }
+        .tabViewBottomAccessory {
+            Text("Word Count: \(script.split { $0.isWhitespace }.count)")
+                .font(.headline)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.ultraThinMaterial)
+        }
+    }
+}
+
+#Preview {
+    TheTabView()
+}
