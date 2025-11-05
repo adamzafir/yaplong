9ed54038290ca69fd8b416ee9d23d669c447a925
Chan Yap Long <chan.yap.long@sji.edu.sg>
2025-11-05 15:03:59 +0800

    Initial Commit

---
+++ lyiglyuvgylu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lyiglyuvgylu
+//
+//  Created by Chan Yap Long on 5/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
