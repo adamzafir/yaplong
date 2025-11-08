8447b1dd0db5db93f229ad785a753b00579ae40f
adamzafir <adam_zafir_mohammed_sudhir+github@s2024.ssts.edu.sg>
2025-11-08 14:38:02 +0800

    stt code

--- lyiglyuvgylu/Views/Screen3.swift
+++ lyiglyuvgylu/Views/Screen3.swift
@@ -1,15 +1,59 @@
-//
-//  Screen3.swift
-//  lyiglyuvgylu
-//
-//  Created by Chan Yap Long on 7/11/25.
-//
+//taken from speech workshop
 
 import SwiftUI
+import AVFoundation
+import Speech
 
 struct Screen3: View {
+    let synthesiser = AVSpeechSynthesizer()
+
+    let audioEngine = AVAudioEngine()
+    let speechRecogniser = SFSpeechRecognizer(locale: .current)
+    @State var transcription = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("Recognise") {
+                
+                SFSpeechRecognizer.requestAuthorization { status in
+                    guard status == .authorized else { return }
+                }
+
+                Task {
+                    let micGranted = await AVAudioApplication.requestRecordPermission()
+                    guard micGranted else { return }
+                }
+
+                guard let recogniser = speechRecogniser, recogniser.isAvailable else { return }
+
+                
+                let audioSession = AVAudioSession.sharedInstance()
+                try? audioSession.setCategory(.record, mode: .measurement)
+                try? audioSession.setActive(true)
+
+                
+                let request = SFSpeechAudioBufferRecognitionRequest()
+                request.shouldReportPartialResults = true
+
+                let inputNode = audioEngine.inputNode
+                let format = inputNode.outputFormat(forBus: 0)
+
+                inputNode.removeTap(onBus: 0)
+                inputNode.installTap(onBus: 0, bufferSize: 1024, format: format) { buffer, _ in
+                    request.append(buffer)
+                }
+                audioEngine.prepare()
+                try? audioEngine.start()
+
+                recogniser.recognitionTask(with: request) { result, _ in
+                    if let result {
+                        transcription = result.bestTranscription.formattedString
+                    }
+                }
+            }
+            Text(transcription)
+        }
+        .padding()
     }
 }
 
